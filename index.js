836428766a66911906564d7f5129d6d441f93d52
Rayan Abarkan <rayan.abarkan@student.vinci.be>
2022-05-04 21:38:13 +0200

    Add library discord together & implement the draw command

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
-
-const bot = new Client({
+const { DiscordTogether } = require('discord-together');
+const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS, // global event on the server
         Intents.FLAGS.GUILD_MESSAGES, // global event message
     ]
 });
+client.discordTogether = new DiscordTogether(client);
 
-bot.on("ready", ()=>{
+
+client.on("ready", ()=>{
     console.log("Started");
 });
 
-bot.on("guildMemberAdd", member =>{
+client.on("guildMemberAdd", member =>{
     const channelId = 935852118152208387;
     const welcomeMessage = `Bienvenue à toi guerrier <@${member.id}>!`;
     member.guild.channels.fetch(channelId).then(channel =>{
@@ -21,13 +23,21 @@ bot.on("guildMemberAdd", member =>{
 });
 
 
-bot.on("messageCreate",  message =>{
+client.on("messageCreate",  message =>{
     if(message.author.bot) return;
 
     if(message.content.toLowerCase()==='!kingdom'){
         message.channel.send('Rejoins Shin !');
     }
+
+    if (message.content === '!draw') {
+        if(message.member.voice.channel) {
+            client.discordTogether.createTogetherCode(message.member.voice.channel.id, 'sketchheads').then(async invite => {
+                return message.channel.send(`${invite.code}`);
+            });
+        };
+    };
 });
 
 
-bot.login(process.env.TK);
+client.login(process.env.TK);
